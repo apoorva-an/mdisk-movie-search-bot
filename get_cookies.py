ca45c6499f44e16f834290a239b75736449c189e
satyam5465 <104612279+satyam5465@users.noreply.github.com>
2022-04-30 14:57:46 +0530

    Update get_cookies.py

--- get_cookies.py
+++ get_cookies.py
@@ -1,23 +1,23 @@
 # (c) @Magic121
 
-from core.login import pdisk_login
+from core.login import mdisk_login
 
 PDisk_DB = {}
 
 
 async def get_cookies(username: str, password: str) -> str:
-    if not PDisk_DB:
-        user_id, cookies = await pdisk_login(username, password)
-        PDisk_DB["cookies"] = cookies
-        PDisk_DB["user_id"] = user_id
-        PDisk_DB["username"] = username
-        PDisk_DB["password"] = password
+    if not MDisk_DB:
+        user_id, cookies = await mdisk_login(username, password)
+        Mdisk_DB["cookies"] = cookies
+        Mdisk_DB["user_id"] = user_id
+        Mdisk_DB["username"] = username
+        MDisk_DB["password"] = password
 
-    return PDisk_DB["cookies"]
+    return MDisk_DB["cookies"]
 
 
 async def set_cookies(data: dict):
-    PDisk_DB["username"] = data["username"]
-    PDisk_DB["password"] = data["password"]
-    PDisk_DB["user_id"] = data["user_id"]
-    PDisk_DB["cookies"] = data["cookies"]
+    MDisk_DB["username"] = data["username"]
+    MDisk_DB["password"] = data["password"]
+    MDisk_DB["user_id"] = data["user_id"]
+    MDisk_DB["cookies"] = data["cookies"]
